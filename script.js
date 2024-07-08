d1bbb06fefcce660d953aabb865d9a9ba8f001f5
Lerato24321 <123421998+Lerato24321@users.noreply.github.com>
2024-07-08 02:32:43 +0200

    added a prompt asking for first name using JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = prompt ("What is your first name?");
+if (firstName.length>0 ) {
+    alert(`Welcome to Jay's Burgers, ${firstName}. Please place your order 😊`);
+} else {
+    alert(`Please type first name before proceeding. ☹️`);
+}
